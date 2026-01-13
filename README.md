daee083a5a7e4741dc0d12fd1c9663aa2f26d6a4
codefaux <codefaux@solacenet.org>
2026-01-13 13:50:10 -0800

    Initial commit. Split from codefaux/webhook-acquisition-initiator

---
+++ README.md
@@ -0,0 +1,3 @@
+# fauxjson
+
+Lightweight functions for loading and saving Dict and List objects.
---
+++ fauxjson/__init__.py
@@ -0,0 +1,3 @@
+from .cfjson import delete_item_file, ensure_dir, load_item, save_item
+
+__all__ = ("load_item", "delete_item_file", "save_item", "ensure_dir")
